b406b6d90902f905dda2a7c24a1f9ee676ebe25d
shashank <shashankhebbarm@gmail.com>
2023-12-25 14:05:10 +0530

    Refactor TrailerVideo component and add useMovieTrailer hook

--- src/components/TrailerVideo.js
+++ src/components/TrailerVideo.js
@@ -1,26 +1,7 @@
-import { useEffect, useState } from "react";
-import { GET_MOVIES_OPTIONS } from "../utils/constants";
+import  useMovieTrailer  from "../hooks/useMovieTrailer";
 
 const TrailerVideo = ({ movieId }) => {
-  const [trailer, setTrailer] = useState(null);
-
-  const fetchTrailer = async () => {
-    const response = await fetch(
-      `https://api.themoviedb.org/3/movie/${movieId}/videos?language=en-US`,
-      GET_MOVIES_OPTIONS
-    );
-    const data = await response.json();
-    const trailer = data.results.find(
-      (video) => video.type === "Trailer" && video.site === "YouTube"
-    );
-    console.log("trailer", trailer);
-    const youtubeUrl = `https://www.youtube.com/embed/${trailer.key}?autoplay=1&controls=0&showinfo=0&mute=1`;
-    setTrailer(youtubeUrl);
-  };
-
-  useEffect(() => {
-    fetchTrailer();
-  }, []);
+const trailer = useMovieTrailer(movieId);
 
   return (
     <div>
