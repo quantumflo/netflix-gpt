11614467ef783f02a3fe5f84626a75a9a4c498f1
shashank <shashankhebbarm@gmail.com>
2023-12-26 23:31:17 +0530

    Add conditional rendering for movie poster and add gptMovies and movieNames to gptSlice

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -2,6 +2,7 @@ import {MOVIE_POSTER} from '../utils/constants';
 
 const MovieCard = ({movie}) => {
     const {poster_path} = movie;
+    if(!poster_path) return null;
   return (
     <div className='flex-shrink-0 zoom'>
         <img className="w-[10rem]" src={MOVIE_POSTER + poster_path} alt={movie.title} />
--- src/redux/gptSlice.js
+++ src/redux/gptSlice.js
@@ -4,13 +4,19 @@ const gptSlice = createSlice({
     name: 'gpt',
     initialState: {
         gptSearch: false,
+        movieNames: null,
+        gptMovies: null
     },
     reducers: {
         toggleGptSearch: (state, action) => {
             state.gptSearch = !state.gptSearch;
         },
+        addGptMovies: (state, action) => {
+            state.gptMovies = action.payload.gptMovies;
+            state.movieNames = action.payload.movieNames;
+        }
     }
 });
 
-export const { toggleGptSearch } = gptSlice.actions;
+export const { toggleGptSearch, addGptMovies } = gptSlice.actions;
 export default gptSlice.reducer;
