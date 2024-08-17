11099ece78c17d08506b37f66d29ac24dab8ed39
shashank <shashankhebbarm@gmail.com>
2024-08-18 00:14:06 +0530

    js doc

--- src/utils/openai.js
+++ src/utils/openai.js
@@ -1,5 +1,8 @@
 import OpenAI from 'openai';
 
+/**
+ * OpenAI GPT-3 API client
+ */
 export const openai = new OpenAI({
   apiKey: process.env.REACT_APP_OPENAI_GPT_API_KEY, 
   dangerouslyAllowBrowser: true,
